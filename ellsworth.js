d847b55056694f0b235db7f6d0e40cd61e9a19e9
Mali Akmanalp <mali@akmanalp.com>
2014-05-11 02:31:41 -0400

    Make square size closer to reference.

--- ellsworth.js
+++ ellsworth.js
@@ -1,5 +1,5 @@
 var amount = 8;
-var size = 80;
+var size = 100;
 var animate = false;
 var colors = [  "#ece9e0", "#e9e9df",   "#ead313",  "#cf5313", "#d75766", "#c71a1c", "#825095", "#311d36", "#315fac", "#1a226b", "#8ab565", "#152525"];
 //              offwhite1,  offwhite2,  lemonyellow, orangered, pink, redred, purple, darkpurple, lighterblue, darkblue, pukegreen, darkgreen
