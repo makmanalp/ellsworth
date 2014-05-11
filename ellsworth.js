beea9ec093b0ad2055b599e436d2c5541ef67adf
Mali Akmanalp <mali@akmanalp.com>
2014-05-11 02:28:43 -0400

    Make colors a tad random

--- ellsworth.js
+++ ellsworth.js
@@ -4,12 +4,21 @@ var animate = false;
 var colors = [  "#ece9e0", "#e9e9df",   "#ead313",  "#cf5313", "#d75766", "#c71a1c", "#825095", "#311d36", "#315fac", "#1a226b", "#8ab565", "#152525"];
 //              offwhite1,  offwhite2,  lemonyellow, orangered, pink, redred, purple, darkpurple, lighterblue, darkblue, pukegreen, darkgreen
 
+
+
+function randomColor(){
+    return new Color(colors[Math.floor(Math.random() * colors.length)]);
+}
+
+
 // Draw squares
 for (var i = 0; i < amount; i++) {
     for (var j = 0; j < amount; j++) {
         var rect = new Rectangle(i * size, j * size, size, size);
         var path = new Path.Rectangle(rect);
-        path.fillColor = colors[Math.floor(Math.random() * colors.length)];
+        path.fillColor = randomColor() + 0.1 * Math.random();
+        path.strokeColor = randomColor();
+        path.strokeWidth = 0.2;
     }
 }
 
